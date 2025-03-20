1185e34cc08f057ff51d759b9fa5e60826dd11ee
Sneha <snehasreeram@live.com>
2025-03-19 22:44:08 -0400

    Final

--- Day1_BandNameGen.py
+++ Day1_BandNameGen.py
@@ -1,1 +1,6 @@
-#test#
+#Band Name Generator#
+
+city = input("Which city where you born in?\n").title()
+pet = input("What is your pet's name?\n").title()
+print(f"Your band name could be: {city} {pet}\n")
+
