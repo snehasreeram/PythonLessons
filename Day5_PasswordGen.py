0b2210574e3a3069e59f05574883b9a420da1a97
Sneha <snehasreeram@live.com>
2025-03-20 23:36:04 -0400

    Final

---
+++ Day5_PasswordGen.py
@@ -0,0 +1,25 @@
+import random as rand
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+blank_pass = []
+for i in range (0, nr_letters):
+    blank_pass += rand.choice(letters)
+
+for j in range (0, nr_symbols):
+    blank_pass += rand.choice(symbols)
+
+for k in range (0, nr_numbers):
+    blank_pass += rand.choice(numbers)
+
+rand.shuffle(blank_pass)
+
+new_password = ''.join(map(str, blank_pass))
+print(f"You new password is: {new_password}")
